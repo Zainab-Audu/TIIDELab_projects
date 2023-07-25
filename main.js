9c8516df9df7f588c2e5bcc32f05cc111b3d8315
Zaynab <Zaynabaudu2@gmail.com>
2023-07-25 12:29:08 +0100

    1st Commit

---
+++ main.js
@@ -0,0 +1,30 @@
+// initialize and add the map
+function initMap() {
+  // your location
+  const loc = { lat: 42.361145, lng: -71.057083 };
+  // centered map on location
+  const map = new google.maps.Map(document.querySelector(".map"), {
+    zoom: 14,
+    center: loc,
+  });
+  // The marker, position at location
+  const marker = new google.maps.Maker({ position: loc, map: map });
+}
+
+
+// Smooth Scrolling
+
+$('#navbar a, .btn').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+}
+});
